21a111c327f06325fb38183ebf014d3b86a35ee3
v2fingers <sjmshelton@proton.me>
2026-09-16 11:11:44 +1200

    Feat: Logging allocation size in mem_calloc

--- Source/Runtime/Core/Memory/Memory.c
+++ Source/Runtime/Core/Memory/Memory.c
@@ -58,6 +58,7 @@ void *mem_calloc(u64 count, u64 size) {
   }
 
   stats.total_allocated += total;
+  LOG_INFO("Memory Allocated: %p (%lu bytes)", chunk, size);
   mem_zero(chunk, total);
 
   return chunk;
